b80a3a6ecc40365132290fcc7856d488bd5d5ccd
Baldo <barrosoub@protonmail.com>
2024-06-20 19:54:26 -0600

    the project was initialized correctly

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import  Button from '@mui/material/Button'
+import React from 'react'
+
+
+const Home = () => {
+  return (
+    <>
+    <Button>
+    This is Home
+
+    </Button>
+   
+      
+    </>
+  )
+}
+
+export default Home
