e03d078ce8c8a807b79076f819dd7f62af85717a
Baldo <barrosoub@protonmail.com>
2024-06-22 20:22:29 -0600

    The dependecies are installed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import Container from '@mui/material/Container'
-import OneOffCustomization from './components/OneOffCustomization'
 import Typography from '@mui/material/Typography'
 
 const Home = () => {
   return (
     <>
-      <Container sx={{display: 'flex', flexDirection: 'column'}}>
-      <Typography sx={{fontWeight: 1000}} variant='h1'>This is Home </Typography>  
-        <OneOffCustomization />
+      <Container sx={{ display: 'flex', flexDirection: 'column' }}>
+        <Typography sx={{ fontWeight: 1000 }} variant='h2'>This is Home </Typography>
       </Container>
     </>
   )
